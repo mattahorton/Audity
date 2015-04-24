3d6945a79161d3fa07d772a13472405d47429b6b
Andrew Forsyth <forsythac@gmail.com>
2015-04-23 17:36:32 -0700

    limit signature to 22 characters

--- Audity/AUDSettingsViewController.m
+++ Audity/AUDSettingsViewController.m
@@ -75,4 +75,16 @@
     UITextField *sig = (UITextField *)sender;
     [defaults setValue:sig.text forKey:@"defaultSig"];
 }
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    // Prevent crashing undo bug – see note below.
+    if(range.length + range.location > textField.text.length)
+    {
+        return NO;
+    }
+    
+    NSUInteger newLength = [textField.text length] + [string length] - range.length;
+    return newLength <= 22;
+}
+
 @end
